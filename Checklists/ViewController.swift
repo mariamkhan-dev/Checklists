13673ef20808a9a5c3c57fccffc22da853f05773
mariamkhan-dev <mariamkhanofficial@gmail.com>
2021-02-19 22:56:03 -0500

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Mariam Khan following UIKit Apprentice Textbook
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
