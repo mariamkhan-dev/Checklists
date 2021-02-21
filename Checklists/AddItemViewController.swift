5e449ef840841c2301d1f0844ac75665879c787c
mariamkhan-dev <mariamkhanofficial@gmail.com>
2021-02-20 20:38:04 -0500

    Added text field to Add Item view controller

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -2,16 +2,25 @@
 //  AddItemViewController.swift
 //  Checklists
 //
-//  Created by md khan on 2/20/21.
+//  Created by m khan on 2/20/21.
 //
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
-  override func viewDidLoad() {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    @IBOutlet weak var textField: UITextField!
+    override func viewDidLoad() {
     super.viewDidLoad()
     navigationItem.largeTitleDisplayMode = .never
   }
+    
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+
     
     // MARK: - Actions
     @IBAction func cancel() {
@@ -19,6 +28,32 @@ class AddItemViewController: UITableViewController {
     }
 
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
       navigationController?.popViewController(animated: true)
+        
     }
+    
+    // MARK: - Table View Delegates
+    override func tableView(
+        _ tableView: UITableView,
+        willSelectRowAt indexPath: IndexPath
+    ) -> IndexPath? {
+        return nil
+    }
+    
+    
+    // MARK: - Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+      let oldText = textField.text!
+      let stringRange = Range(range, in: oldText)!
+      let newText = oldText.replacingCharacters(in: stringRange, with: string)
+      doneBarButton.isEnabled = !newText.isEmpty
+      return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+      doneBarButton.isEnabled = false
+      return true
+    }
+
 }
